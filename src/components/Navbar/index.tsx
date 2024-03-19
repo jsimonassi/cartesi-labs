f777bd685b204e1ae7a2484eb761fbfa21df9fe4
gustavo luppi <gustavoluppi@id.uff.br>
2024-03-19 00:41:16 -0300

    fixing home return

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -8,7 +8,7 @@ export const Navbar = () => {
 		<nav className="bg-white  fixed w-full z-20 top-0 start-0 border-b border-gray-200 ">
 			<div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
 				
-				<a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
+				<a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
 					<img src={Logo} className="h-8" alt="Flowbite Logo"></img>
 					<span className=" text-h4  ">cartesi</span>
 				</a>
