dc0e0a97b3c6e3deb93476f4abf4f902f4e0358d
bernardocerq <berbatovrobben@gmail.com>
2024-03-20 00:05:21 -0300

    config: router configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,12 @@
 import React from "react";
 import "./App.css";
 import BaseBtn from "./components/buttons/BaseBtn";
+import Router from "./router";
+
 
 function App() {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<p>
-          Cartesi Labs
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-          Learn React
-				</a>
-
-				<h1 className="undeline bg-red-500 text-h4">
-          Hello world!
-				</h1>
-				<BaseBtn variant="contained" size="md" color="black">Botao</BaseBtn>
-			</header>
-		</div>
+		<Router />
 	);
 }
 
